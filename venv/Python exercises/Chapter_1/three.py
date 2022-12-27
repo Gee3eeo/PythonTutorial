2e8b5782c73243ee4fc5a81e085c69aa3c13d01d
Gee3eeo <isanborgerald@gmail.com>
2022-12-27 02:04:28 -0800

    exercises done

---
+++ venv/Python exercises/Chapter_1/three.py
@@ -0,0 +1,2 @@
+num = int(input('Enter a number: '))
+print('The square of number is', num*num, sep= ' ', end='.')
---
+++ venv/Python exercises/Chapter_1/two.py
@@ -0,0 +1,5 @@
+x = 512
+y = 282
+z = 47.48
+a = 5
+print('It is roughly', (x-y)/(z+a))
